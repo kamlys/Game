dae00ba018e8dc126b906133feb259ffc0ba4275
= <faraon5593@gmail.com>
2016-01-17 15:08:23 +0100

    missing files fix

---
+++ WebGame/Game.GUI/ViewModels/Market/ItemMarketViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+namespace Game.GUI.ViewModels.Market
+{
+    public class ItemMarketViewModel
+    {
+        public int ID { get; set; } 
+        public int User_ID { get; set; }
+        public string User_Login { get; set; }
+        public int Product_ID { get; set; }
+        public string Product_Name { get; set; }
+        public int Number { get; set; }
+        public int Price { get; set; }
+        public bool TypeOffer { get; set; }
+    }
+}
---
+++ WebGame/Game.GUI/ViewModels/Product/UserProduct/ItemUserProductViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Game.GUI.ViewModels.Product.UserProduct
+{
+    public class ItemUserProductViewModel
+    {
+        public int ID { get; set; }
+        public int User_ID { get; set; }
+        public string User_Login { get; set; }
+        public int Product_ID { get; set; }
+        public string Product_Name { get; set; }
+        public int Value { get; set; }
+        public int Price { get; set; }
+    }
+}
