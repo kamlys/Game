b48429a17b15dd6b378baede75d20cb4f4ae1bac
kamlys <lysek.kamil@gmail.com>
2016-01-17 13:16:24 +0100

    ViewModels File

---
+++ WebGame/Game.GUI/ViewModels/Building/ItemBuildingViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Game.GUI.ViewModels.Building
+{
+    public class ItemBuildingViewModel
+    {
+        public int ID { get; set; }
+        public string BuildingName { get; set; }
+        public int Price { get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public int DealID { get; set; }
+        public int DestPrice { get; set; }
+        public int Percent_price_per_lvl { get; set; }
+        public int Percent_product_per_lvl { get; set; }
+        public int Product_ID { get; set; }
+        public string Product_Name { get; set; }
+        public int Product_per_sec { get; set; }
+        public string Alias { get; set; }
+        public int BuildingTime { get; set; }
+        public int DestructionTime { get; set; }
+
+    }
+}
---
+++ WebGame/Game.GUI/ViewModels/Ranking/RankingViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Game.GUI.ViewModels.Ranking
+{
+    public class RankingViewModel
+    {
+        public int UserID { get; set; }
+        public string User_Login { get; set; }
+        public int UserDolar { get; set; }
+        public int Position { get; set; }
+    }
+}
